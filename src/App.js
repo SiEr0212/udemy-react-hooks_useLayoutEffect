4d35ec7987281fbc5ef3a8e1641e58e3c88c1683
SiEr0212 <simonertel@gmx.de>
2021-05-05 20:20:03 +0200

    added useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [val, setVal] = useState(2)
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div>HI</div>
       </header>
     </div>
   );
