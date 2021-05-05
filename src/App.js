42c5a22f78396c888a4654013fc4a486ef81dd76
SiEr0212 <simonertel@gmx.de>
2021-05-05 20:27:06 +0200

    added nultiply btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [val, setVal] = useState(2)
+  const [val, setVal] = useState(2);
   return (
     <div className="App">
       <header className="App-header">
-        <div>HI</div>
+        <div>{val}</div>
+        <button onClick={() => setVal(val * 10)}>10*</button>
       </header>
     </div>
   );
